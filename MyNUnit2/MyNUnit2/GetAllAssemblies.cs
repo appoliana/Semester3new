594aaeb28397093238c3c62a483cfab7ea864fa0
appoliana <polinapikkio@gmail.com>
2019-05-01 13:44:14 +0300

    wow mynunit

---
+++ MyNUnit2/MyNUnit2/GetAllAssemblies.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNUnit
+{
+    class GetAllAssemblies
+    {
+        /// <summary>
+        /// Метод, который извлекает из сборки все exe и dll файлы.
+        /// </summary>
+        /// <param name="parth"></param>
+        public (List<Assembly>, string) GetAll(string parth)
+        {
+            var arrayOfFiles = new string[100];
+            var listOfAssemblies = new List<Assembly>();
+            if (Directory.Exists(parth))
+            {
+                arrayOfFiles = Directory.GetFiles(parth, "*.exe", SearchOption.AllDirectories)
+                    .Concat(Directory.GetFiles(parth, "*.dll", SearchOption.AllDirectories)).ToArray();
+                /*for(int i = 0; i < arrayOfFiles.Length; ++i)
+                {
+                    Console.WriteLine(arrayOfFiles[i]);
+                }
+                */
+                try
+                {
+                    for (int i = 0; i < arrayOfFiles.Length; ++i)
+                    {
+                        listOfAssemblies.Add(Assembly.LoadFrom(arrayOfFiles[i]));
+                    }
+                    return (listOfAssemblies, "Все в порядке, ошибок нет!");
+                }
+                catch (Exception ex)
+                {
+                    return (listOfAssemblies, "Произошла ошибка в загрузке сборки" + ex);
+                }
+            }
+            return (listOfAssemblies, "Указанного пути не существует.");
+        }
+    }
+}
---
+++ MyNUnit2/MyNUnit2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Right a parth to assembly...");
+            string parth = Console.ReadLine();
+            string messageAboutWorkingAssemblies = "";
+            var listOfAssemblies = new List<Assembly>();
+            GetAllAssemblies getter = new GetAllAssemblies();
+            (listOfAssemblies, messageAboutWorkingAssemblies) = getter.GetAll(parth);
+            if (messageAboutWorkingAssemblies == "Все в порядке, ошибок нет!")
+            {
+                Console.WriteLine("Assemblies are ready, tests begining to work.");
+                RunTestsInAssembly run = new RunTestsInAssembly();
+
+                for (int i = 0; i < listOfAssemblies.Count; ++i)
+                {
+                    //Console.WriteLine("Assembly " + listOfAssemblies[i].GetName(false) + " " + " is watching now.");
+                    //if (listOfAssemblies[i].GetName(false).ToString().Contains("ComparatorTests"))
+                    run.RunTests(listOfAssemblies[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine(messageAboutWorkingAssemblies);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+// найти сборки, запустить тесты в сборках
+// информацию вывести на экран
+
+// получить информацию о сборке с помощью рефлексии
+// найти методы, помеченные анотацией Test, Expected и Ignore
+// параллельно запустить тесты
+
+// обработать тест
+// получить результат работы теста
+
+// написать тесты на систему тестирования
+
+
+
+// ?? что такое методы before и after
